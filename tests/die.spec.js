8a576233d7a6bac5e7f779c8f51d5dd044b6b912
Nick Sandonato <nick.sandonato@gmail.com>
2016-01-04 20:18:33 -0500

    Add more unit tests surrounding dice notation

--- tests/die.spec.js
+++ tests/die.spec.js
@@ -1,25 +1,25 @@
 var tape = require('tape');
 var Die = require('../lib/die');
 
-tape.test('should have a default number of sides', (expect) => {
+tape.test('should have a default number of sides', expect => {
   var die = new Die();
   expect.equal(die.sides, 6);
   expect.end();
 });
 
-tape.test('should allow a specified number of sides', (expect) => {
+tape.test('should allow a specified number of sides', expect => {
   var die = new Die(20);
   expect.equal(die.sides, 20);
   expect.end();
 });
 
-tape.test('should return a result when rolled', (expect) => {
+tape.test('should return a result when rolled', expect => {
   var die = new Die();
   expect.equal(typeof die.roll(), 'number');
   expect.end();
 });
 
-tape.test('should always roll between 1 and the number of sides', (expect) => {
+tape.test('should always roll between 1 and the number of sides', expect => {
   var die = new Die(1);
   for (var i = 0; i < 100; i++) {
     // Statistically reson
@@ -32,12 +32,40 @@ tape.test('should always roll between 1 and the number of sides', (expect) => {
   expect.end();
 });
 
-tape.test('should have a parse function with a default operation', (expect) => {
+tape.test('should have a parse function with a default operation', expect => {
   expect.equal(typeof Die.parse, 'function');
   var parsed = Die.parse();
   expect.ok(Array.isArray(parsed));
   expect.equal(parsed.length, 1);
   expect.equal(parsed[0].sides, 6);
   expect.ok(parsed[0].side >= 1 && parsed[0].side <= 6);
+  expect.end();
+});
+
+tape.test('should parse expressions, defaulting to one die', expect => {
+  expect.equal(typeof Die.parse, 'function');
+  var parsed = Die.parse('d20');
+  expect.ok(Array.isArray(parsed));
+  expect.equal(parsed.length, 1);
+  expect.equal(parsed[0].sides, 20);
+  expect.end();
+});
+
+tape.test('should parse expressions of dice notation format', expect => {
+  expect.equal(typeof Die.parse, 'function');
+  var parsed = Die.parse('5d20');
+  expect.ok(Array.isArray(parsed));
+  expect.equal(parsed.length, 5);
+  var wrongSides = parsed.some(die => {
+    if (die.sides !== 20) {
+      return true;
+    }
+  });
+  if (wrongSides) {
+    expect.fail('A die didn\'t have 20 sides');
+  }
+  else {
+    expect.pass();
+  }
   expect.end();
 });
